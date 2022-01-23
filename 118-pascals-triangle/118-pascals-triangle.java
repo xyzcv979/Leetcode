38c56b9b13bc93962b48465029a530f893a70a7f
Alex <avon1462@gmail.com>
2022-01-23 18:05:21 -0500

    Time: 1 ms (45.97%), Space: 38.3 MB (44.43%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascalsTriangle = new ArrayList<>();
+        List<Integer> currRow = new ArrayList<>();
+        for(int row = 0; row < numRows; row++){
+            currRow.add(0,1);
+            for(int currRowIndex = 1; currRowIndex < row; currRowIndex++){
+                currRow.set(currRowIndex, currRow.get(currRowIndex) + currRow.get(currRowIndex + 1));
+            }
+            pascalsTriangle.add(new ArrayList<>(currRow)); // create new object that is a copy of currRow, and add to triangle
+        }
+        return pascalsTriangle;
+    }
+}
