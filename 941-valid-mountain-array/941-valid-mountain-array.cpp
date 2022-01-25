88cb6aac3d41a4eac2918370e90c90bb679eca90
Alex <avon1462@gmail.com>
2022-01-25 01:17:46 -0500

    Time: 40 ms (36.68%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,3 +1,5 @@
+// Reference: https://leetcode.com/problems/valid-mountain-array/discuss/451553/C%2B%2B-Simple-Solution-with-Explanations-for-newbies-less3
+
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
@@ -6,18 +8,15 @@ public:
         
         int ptr = 1;
         while(ptr < arr.size()){
-            if(arr[ptr] <= arr[ptr - 1]){
+            if(arr[ptr] <= arr[ptr - 1])
                 break;
-            }
             ptr++;
         }
-        if(ptr == arr.size() || ptr == 1){
+        if(ptr == arr.size() || ptr == 1)
             return false;
-        }
         while(ptr < arr.size()){
-            if(arr[ptr] >= arr[ptr - 1]){
+            if(arr[ptr] >= arr[ptr - 1])
                 break;
-            }
             ptr++;
         }
         
