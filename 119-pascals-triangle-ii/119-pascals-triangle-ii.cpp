d79a8021fe2a105f8b5368e4692f680da94fbdbb
Alex <avon1462@gmail.com>
2022-01-28 20:59:25 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (79.67%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> triangle;
+        vector<int> cur, prev;
         for(int i = 0; i <= rowIndex; i++){
-            vector<int> row(i + 1, 1);
+            cur.assign(i + 1, 1);
             for(int j = 1; j < i; j++){
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]; 
+                cur[j] = prev[j-1] + prev[j];
             }
-            triangle.push_back(row);
+            prev = move(cur);
         }
-        return triangle[rowIndex];
+        return prev;
     }
 };
