d99b36bbc21ffd9e675731e6b5e7c52833cc6d16
Alex <avon1462@gmail.com>
2022-02-09 21:02:46 -0500

    Time: 16 ms (5.12%), Space: 6.1 MB (13.88%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<bool> dp(n + 1, false);
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j < i; j++){
+                if(i % j == 0 && dp[i - j] == 0)
+                    dp[i] = true;
+            }
+        }
+        return dp[n];
+    }
+};
