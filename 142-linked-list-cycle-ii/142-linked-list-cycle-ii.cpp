06fa4fa9e8bc4821700dfc4230e864bc9f2be30c
Alex <avon1462@gmail.com>
2022-02-15 11:27:46 -0500

    Time: 8 ms (85.86%), Space: 7.5 MB (90.49%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,14 +9,32 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        set<ListNode*> visited;
-        ListNode *curr = head;
-        while(curr != NULL){
-            if(visited.count(curr))
-                return curr;
-            visited.insert(curr);
-            curr = curr->next;
+        // Floyd's tortoise and hare
+        // 1. Check if cycle is present
+        // 2. Init 2 more ptrs
+        //     1 at head, 1 at intersection
+        //     advance ptrs till they meet, this is start of cycle
+        ListNode *fast = head;
+        ListNode *slow = head;
+        bool isCycle = false;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
+                isCycle = true;
+                break;
+            }
         }
+        if(isCycle){
+            ListNode *startPtr = head;
+            ListNode *cyclePtr = fast;
+            while(startPtr != cyclePtr){
+                cyclePtr = cyclePtr->next;
+                startPtr = startPtr->next;
+            }
+            return startPtr;
+        }
+        
         return NULL;
     }
 };
