fbeaa4027eb4a2084d3bd25eb30baf5acef63b7d
Alex <avon1462@gmail.com>
2022-02-22 01:03:01 -0500

    Time: 24 ms (82.00%), Space: 23.8 MB (99.38%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -6,22 +6,15 @@ public:
         
         int n = nums.size();
         
-        vector<int> answer(n);
-        vector<int> prefix(n);
-        vector<int> suffix(n);
-        
-        prefix[0] = 1;
+        vector<int> answer(n,1);
         for(int i = 1; i < n; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+            answer[i] = answer[i - 1] * nums[i-1];
         }
         
-        suffix[n-1] = 1;
-        for(int i = n - 2; i >= 0; i--){
-            suffix[i] = nums[i+1] * suffix[i+1];
-        }
-        
-        for(int i = 0; i < n; i++){
-            answer[i] = prefix[i] * suffix[i];
+        int suffixProduct = 1;
+        for(int i = n - 1; i >= 0; i--){
+            answer[i] = answer[i] * suffixProduct;
+            suffixProduct *= nums[i];
         }
         return answer;
     }
