29f57db58f79486477fc64539f480a9d5b61908a
Alex <avon1462@gmail.com>
2022-02-25 01:02:31 -0500

    Time: 28 ms (46.88%), Space: 10.8 MB (28.74%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-	int lengthOfLongestSubstring(string s) 
-	{
-		unordered_set<char> set;
-    
-		int i = 0, j = 0, n = s.size(), ans = 0;
-    
-		while( i<n && j<n)
-		{
-			if(set.find(s[j]) == set.end()) //If the character does not in the set
-			{
-				set.insert(s[j++]); //Insert the character in set and update the j counter
-				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
-			}
-			else
-			{
-				set.erase(s[i++]); 
-				/*If character does exist in the set, ie. it is a repeated character, 
-				we update the left side counter i, and continue with the checking for substring. */
-			}
-		}
-    
-		return ans;
-	}
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<int> set;
+        int leftPtr = 0, rightPtr = 0;
+        int maxLen = 0;
+        int n = s.size();
+        while(leftPtr < n && rightPtr < n){
+            char curr = s[rightPtr];
+            if(set.find(curr) == set.end()){
+                rightPtr++;
+                maxLen = max(maxLen, rightPtr - leftPtr);
+                set.insert(curr);
+            }
+            else{
+                set.erase(s[leftPtr]);
+                leftPtr++;
+            }
+        }
+        return maxLen;
+    }
 };
