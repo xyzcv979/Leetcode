f6a126d6a9070a6897e486862efd860ed0019e33
Alex <avon1462@gmail.com>
2022-04-10 00:13:56 -0400

    Time: 689 ms (20.00%), Space: 90.8 MB (10.00%) - LeetHub

---
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int modNum = pow(10, 9) + 7;
+        
+        for(int i : nums)
+            pq.push(i);
+        
+        while(k--){
+            int top = pq.top();
+            pq.pop();
+            top++;
+            pq.push(top);
+        }
+        
+        long long res = 1;
+        while(!pq.empty()){
+            res = (res * pq.top()) % modNum;
+            pq.pop();
+        }
+        return (int)res;
+    }
+};
