7d244b4041239a534b06fe15145c02660f679d4f
Alex <avon1462@gmail.com>
2022-09-11 23:49:45 -0400

    Time: 238 ms (15.47%), Space: 36.8 MB (75.26%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
+    int MOD = 1e9 + 7;
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        priority_queue<int, vector<int>, greater<int>> minSpeeds;
-        vector<pair<int,int>> efficiencySpeed;
-        long sumSpeeds = 0, ans = 0;
+        vector<pair<int,int>> effSpeed;
+        priority_queue<int, vector<int>, greater<int>> speedMinHeap;
         
         for(int i = 0; i < n; i++) {
-            efficiencySpeed.push_back({efficiency[i], speed[i]});
+            effSpeed.push_back({efficiency[i], speed[i]});
         }
+        sort(effSpeed.rbegin(), effSpeed.rend());
         
-        sort(efficiencySpeed.rbegin(), efficiencySpeed.rend());
-        
+        long sumSpeed = 0, ans = 0;
         for(int i = 0; i < n; i++) {
-            int currSpeed = efficiencySpeed[i].second;
-            int currEff = efficiencySpeed[i].first;
+            int currSpeed = effSpeed[i].second;
+            int currEff = effSpeed[i].first;
             
-            if(minSpeeds.size() == k) {
-                sumSpeeds -= minSpeeds.top();
-                minSpeeds.pop();
-                
+            if(speedMinHeap.size() == k) {
+                sumSpeed -= speedMinHeap.top();
+                speedMinHeap.pop();
             }
-            sumSpeeds += currSpeed;
-            minSpeeds.push(currSpeed);
-            ans = max(ans, sumSpeeds * currEff);
+            
+            sumSpeed += currSpeed;
+            speedMinHeap.push(currSpeed);
+            
+            ans = max(ans, sumSpeed * currEff);
         }
         
         return ans % MOD;
@@ -31,12 +31,8 @@ public:
 };
 
 /*
-choose k engineers with maximum performance
-performance = sum(speeds) * min(efficiencys)
+sort by efficiency decreasing
+for every efficiency, you get k speeds in a minHeap, popping out minimum speed
+get the max of these performances
 
-
-fix the lowest efficiency first
-sort effeciency in decreasing order
-for every effeciency, we now this is minimum at the time cause of the sortedness, 
-keep a minHeap of speeds, whenever heap is == k size, pop out the loweset speed
 */
