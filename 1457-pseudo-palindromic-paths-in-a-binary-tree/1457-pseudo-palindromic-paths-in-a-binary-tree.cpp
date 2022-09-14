ff395fe4776e0eb9b3ae6b777dec1d17eaa71e8f
Alex <avon1462@gmail.com>
2022-09-14 16:35:52 -0400

    Time: 814 ms (23.82%), Space: 365.5 MB (12.87%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int palinPaths;
+    int pseudoPalindromicPaths (TreeNode* root) {
+        palinPaths = 0;
+        vector<int> counts(10);
+        dfs(root, counts);
+        return palinPaths;
+    }
+    
+    void dfs(TreeNode* root, vector<int> counts) {
+        if(!root)
+            return;
+
+        counts[root->val]++;
+        
+        if(!root->left && !root->right) { // leaf node
+            bool oneOdd = false;
+            for(int i : counts) {
+                if(i % 2 != 0) {
+                    if(oneOdd)
+                        return;
+                    oneOdd = true;
+                }
+            }
+            palinPaths++;
+        }
+        
+        dfs(root->left, counts);
+        dfs(root->right, counts);
+    }
+    
+};
+
+/*
+get all paths
+if path is pseudo palindromic
+   -compute all permuations and check if palindrome?
+   
+*/
