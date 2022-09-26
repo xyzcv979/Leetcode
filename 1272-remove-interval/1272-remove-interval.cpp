5d67cf1ea2ace4aa9875b6f5a3f47e0ee0151bb6
Alex <avon1462@gmail.com>
2022-09-26 18:39:38 -0400

    Time: 101 ms (95.25%), Space: 38.4 MB (42.94%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -8,24 +8,15 @@ public:
         for(int i = 0; i < n; i++) {
             int currStart = intervals[i][0], currEnd = intervals[i][1];
             
-            if(currStart >= removeStart && currEnd <= removeEnd) // currStart and currEnd inside remove interval
-                continue;
-            
-            if(currStart >= removeEnd) { // interval beyond remove interval
+            if(currStart >= removeEnd || currEnd <= removeStart) {
                 ans.push_back({currStart, currEnd});
-            } else if(currEnd <= removeStart) { // interval before remove interval
-                ans.push_back({currStart, currEnd});
-            }
-            
-            if(currStart < removeStart && currEnd >= removeStart) { // currStart before remove interval, currEnd in remove interval
-                ans.push_back({currStart, removeStart});
+            } else {
+                if(currStart < removeStart) {
+                    ans.push_back({currStart, removeStart});
+                }
                 if(currEnd > removeEnd) {
                     ans.push_back({removeEnd, currEnd});
                 }
-            }
-            
-            if(currStart >= removeStart && currStart <= removeEnd && currEnd > removeEnd) { // currStart in remove interval, currEnd after remove interval
-                ans.push_back({removeEnd, currEnd});
             }
         }
         
