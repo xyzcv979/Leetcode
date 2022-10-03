8e4fcba20e5ad88ac197d1f6d19fcb8c712c5b7a
Alex <avon1462@gmail.com>
2022-10-03 12:36:58 -0400

    Time: 72 ms (31.07%), Space: 8.4 MB (46.04%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    const int MOD = 1e9+7;
     vector<vector<int>> memo;
-    
+    const int MOD = 1e9+7;
     int numRollsToTarget(int n, int k, int target) {
-        memo = vector<vector<int>>(n+1, vector<int>(target+1, -1)); // row: n dice, col: 0 to target+1
+        memo =  vector<vector<int>>(n+1, vector<int>(target+1,-1));
         return dfs(n, k, target);
     }
     
     int dfs(int n, int k, int target) {
-        if(target == 0) return n == 0;
-        if (n == 0 || target < 0) return 0;
-
-        // memo
-        if(memo[n][target] != -1) return memo[n][target];
+        if(target == 0) {
+            return n == 0;
+        }
+        if(n == 0 || target < 0) {
+            return 0;
+        }
+        if(memo[n][target] != -1)
+            return memo[n][target];
         
-        int count = 0;
+
+        int possibleWays = 0;
         for(int i = 1; i <= k; i++) {
-            count = (count + dfs(n-1, k, target-i)) % MOD;
+            possibleWays = (possibleWays + dfs(n - 1, k, target-i))%MOD;
         }
         
-        count %= MOD;
-        
-        memo[n][target] = count;
-        return count;
+        memo[n][target] = possibleWays;
+        return possibleWays;
     }
 };
 
 /*
-n dice, k faces
-
-number of combinations that add to target
+states: dice face, sum
 
 
-brute force:
-decrementing dice and target
 
-top down
-memoize brute force
-cache keeps track of current dice number and current sum number
 */
