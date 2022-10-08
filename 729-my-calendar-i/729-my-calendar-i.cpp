d1d015c1b798b7a5f86953f939095f8294d90649
Alex <avon1462@gmail.com>
2022-10-07 20:09:46 -0400

    Time: 244 ms (25.49%), Space: 37.5 MB (94.07%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,27 @@
+class MyCalendar {
+public:
+    vector<pair<int, int>> intervals;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for(auto it : intervals) {
+            int currStart = it.first;
+            int currEnd = it.second;
+            
+            if(max(currStart, start) < min(currEnd, end)) {
+                return false;
+            }
+        }
+        
+        intervals.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
