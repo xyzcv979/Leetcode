03c3f045d7e2d039d0aaeb904461e0c17bfb3a70
Alex <avon1462@gmail.com>
2022-10-17 01:51:35 -0400

    Time: 94 ms (50.42%), Space: 18.8 MB (74.49%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int maxBanana = *max_element(piles.begin(), piles.end());
+        
+        int left = 1, right = maxBanana;
+        while(left <= right) {
+            int midEatingSpeed = left + (right - left) / 2;
+            long int hourSpent = 0;
+            
+            for(int pile : piles) {
+                hourSpent += pile / midEatingSpeed + (pile % midEatingSpeed != 0);
+            }
+            
+            if(hourSpent <= h) {
+                right = midEatingSpeed - 1;
+            } else {
+                left = midEatingSpeed + 1;
+            }
+        }
+        return left;
+    }
+};
+
+/*
+choose some k to subtract from piles[i]
+do this h times to eat all bannanas
+want min k
+
+
+brute force:
+try k == 1
+increment k until you eat all bananas by h
+
+binary search:
+we know that if some num k is valid, k + 1 is also valid
+therefore, we know that if some k is not valid, k - 1 is also not valid
+
+
+*/
