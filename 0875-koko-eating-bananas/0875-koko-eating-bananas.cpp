be427fe4dcb58b679ff3b0778bf8d280b2036db8
Alex <avon1462@gmail.com>
2022-10-17 01:52:24 -0400

    Time: 86 ms (58.67%), Space: 18.9 MB (34.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -34,6 +34,10 @@ try k == 1
 increment k until you eat all bananas by h
 
 binary search:
+using binary search not on sorted array elements but on the eating speed.
+left = 1 banana per hour
+right = max poss bananas eaten per hour which is the max of the piles array
+
 we know that if some num k is valid, k + 1 is also valid
 therefore, we know that if some k is not valid, k - 1 is also not valid
 
