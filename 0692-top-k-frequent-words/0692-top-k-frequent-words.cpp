d7cbc5b06ede6f0e87ab4787668c73117ad49f34
Alex <avon1462@gmail.com>
2022-10-19 12:21:56 -0400

    Time: 22 ms (62.67%), Space: 12.7 MB (56.15%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -3,16 +3,20 @@ public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> wordFreq;
         
-        // love : 2
+        // map string to count ie. "love" : 2
         for(string word : words) {
             wordFreq[word]++;
         }
         
-        // 2 : i, love
+        // Add as a pair of int string to a vector
         vector<pair<int, string>> countToWords;
         for(auto it : wordFreq) {
             countToWords.push_back({it.second, it.first});
         }
+        
+        // Custom sort in descending order
+        // Check if strings are equal, if so, return lexicographical smaller one
+        // Else order by the count of the strings
         sort(countToWords.begin(), countToWords.end(), [](auto &a, auto &b) {
             if(a.first == b.first)
                 return a.second < b.second;
@@ -20,6 +24,7 @@ public:
                 return b.first < a.first;
         });
         
+        // Get top k strings
         vector<string> ans;
         int i = 0;
         while(k) {
@@ -27,6 +32,7 @@ public:
             i++;
             k--;
         }
+        
         return ans;
     }
 };
