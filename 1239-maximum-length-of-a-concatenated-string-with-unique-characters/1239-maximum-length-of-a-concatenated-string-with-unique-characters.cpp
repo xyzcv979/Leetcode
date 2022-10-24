cd6ca92f6f6fda720fcfcbbc4de92ca118a91d4b
Alex <avon1462@gmail.com>
2022-10-24 13:31:33 -0400

    Time: 287 ms (40.05%), Space: 57.9 MB (44.30%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        // Initialize results with an empty string
-        // from which to build all future results
-        vector<string> results = {""};
+        vector<string> validStrings = {""};
         int best = 0;
-        for (string word : arr) {
-            // We only want to iterate through results
-            // that existed prior to this loop
-            int resultsLen = results.size();
-            for (int i = 0; i < resultsLen; i++) {
-                // Form a new result combination and
-                // use a set to check for duplicate characters
-                string newRes = results[i] + word;
-                unordered_set<char> newResSet(newRes.begin(), newRes.end());
-                if (newRes.size() != newResSet.size())
+        
+        for(string s : arr) {
+            int arrSize = validStrings.size();
+            for(int i = 0; i < arrSize; i++) {
+                string concatWords = validStrings[i] + s;
+                unordered_set<char> findDups(concatWords.begin(), concatWords.end());
+                if(concatWords.size() != findDups.size()) {
                     continue;
-
-                // Add valid options to results and
-                // keep track of the longest so far
-                results.push_back(newRes);
-                best = max(best, (int)newRes.size());
+                }
+                validStrings.push_back(concatWords);
+                best = max(best, (int)concatWords.size());
             }
+
         }
         return best;
     }
