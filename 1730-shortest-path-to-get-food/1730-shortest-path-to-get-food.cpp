a2630987ca360051049493bdb67bb1062d934773
Alex <avon1462@gmail.com>
2022-10-30 15:26:18 -0400

    Time: 1901 ms (5.09%), Space: 195.1 MB (5.09%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int getFood(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        queue<vector<int>> que;
+        vector<vector<bool>> seen(n, vector<bool>(m, false));
+        vector<vector<int>> dir = {
+                {-1,0},
+            {0,-1},  {0,1},
+                 {1,0}
+        };
+        
+        // insert starting pos into queue;
+        bool foundStart = false;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == '*') {
+                    que.push({i, j, 0});
+                    foundStart = true;
+                    break;
+                }
+            }
+            if(foundStart)
+                break;
+        }
+        
+        unordered_set<int> stepsFoundFood;
+        while(!que.empty()) {
+            auto front = que.front();
+            int x = front[0], y = front[1], steps = front[2];
+            
+            que.pop();
+            
+            if(x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 'X' || seen[x][y]) {
+                continue;
+            }
+            
+            if(grid[x][y] == '#') {
+                stepsFoundFood.insert(steps);
+            }
+            
+            // Push up,left,right,down
+            for(auto coord : dir) {
+                que.push({x+coord[0], y+coord[1], steps+1});
+            }
+            seen[x][y] = true;
+        }
+        
+        if(stepsFoundFood.size() == 0) {
+            return -1;
+        }
+        
+        int minSteps = INT_MAX;
+        for(int steps : stepsFoundFood) {
+            minSteps = min(minSteps, steps);
+        }
+        
+        return minSteps;
+    }
+};
+
+/*
+state: keep track of steps
+
+does seen array need to keep track of steps? 
+or is it just true/false visited
+*/
