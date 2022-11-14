ac7e9549159feac513767ccfb0969f3f3e65b44d
Alex <avon1462@gmail.com>
2022-11-14 14:13:58 -0500

    Time: 667 ms (9.73%), Space: 14.1 MB (95.67%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int&n){
-        visited[index]=true;
-        int result=0;
-        for(int i=0;i<n;i++)
-            if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
-                result +=(dfs(stones,i,visited,n) + 1);
-        return result;
-    }
-    int removeStones(vector<vector<int>>&stones) {
-        int n = stones.size();
-        vector<bool>visited(n,0);
-        int result=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]){continue;}
-            result+=dfs(stones,i,visited,n);
+    vector<bool> visited;
+    int removeStones(vector<vector<int>>& stones) {
+        visited = vector<bool>(stones.size());
+        int connectedComponents = 0;
+        for(int i = 0; i < stones.size(); i++) {
+            if(!visited[i]) {
+                dfs(stones, visited, i);
+                connectedComponents++;
+            }
         }
-        return result;
+        return stones.size() - connectedComponents;
+    }
+    
+    void dfs(vector<vector<int>>& stones, vector<bool> &visited, int i) {
+        visited[i] = true;
+        
+        for(int j = 0; j < stones.size(); j++) {
+            // visited contains this curr pair
+            if(visited[j]) continue;
+            if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                dfs(stones, visited, j);
+            }
+        }
     }
 };
 
