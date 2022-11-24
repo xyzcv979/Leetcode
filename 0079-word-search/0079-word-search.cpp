f716ed856588340dbb263b4d23b6706a4afa0f15
Alex <avon1462@gmail.com>
2022-11-24 14:07:39 -0500

    Time: 927 ms (37.55%), Space: 8.1 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
+    int n, m;
     bool exist(vector<vector<char>>& board, string word) {
-        // find starting position for word[0]
+        n = board.size(), m = board[0].size();
         
-        for(int i = 0; i < board.size(); i++) {
-            for(int j = 0; j < board[i].size(); j++) {
-                if(word[0] == board[i][j]) {
-                    bool isTrue = false;
-                    isTrue = dfs(board, word, i, j, 0);
-                    if(isTrue) return true;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == word[0]) {
+                    if(dfs(board, word, 0, i, j))
+                        return true;
                 }
             }
         }
         return false;
-    }
+    }  
     
-    bool dfs(vector<vector<char>>& board, string word, int row, int col, int index) {
-        if(index == word.size()) return true;
-        if(row < 0 || row == board.size() || col < 0 ||
-           col == board[0].size() || word[index] != board[row][col]) return false;
+    bool dfs(vector<vector<char>>& board, string word, int idx, int row, int col) {
+        if(idx == word.size())
+            return true;
         
+        if(row < 0 || row == n || col < 0 || col == m || board[row][col] != word[idx])
+            return false;
+
+        // Backtrack
         char temp = board[row][col];
-        board[row][col] = ' ';
-        // dfs on left, right, up, down
-        bool isValid = dfs(board, word, row,col+1, index+1) || dfs(board, word, row+1,col, index+1)
-                        || dfs(board, word, row, col-1, index+1) || dfs(board, word, row - 1, col, index+1);
-        board[row][col] = temp; // backtrack
+        board[row][col] = '.';
+        bool isValid;
+        isValid = dfs(board,word,idx+1,row+1, col) ||                   dfs(board,word,idx+1,row-1, col) || 
+            dfs(board,word,idx+1,row, col+1) ||
+            dfs(board,word,idx+1,row, col-1);
+
+        board[row][col] = temp;
         
         return isValid;
     }
 };
+
+/*
+backtracking
+try one char, erase it and try another char
+all 4 directions
+*/
