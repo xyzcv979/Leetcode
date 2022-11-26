f0095760f7fdd60950a1f1b3a06a95af958deca1
Alex <avon1462@gmail.com>
2022-11-26 16:16:37 -0500

    Time: 281 ms (66.26%), Space: 84.2 MB (6.97%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -28,11 +28,12 @@ public:
         if(memo.find(pos) != memo.end())
             return memo[pos];
         
-        // Binary search: return iterator to first ele which is >= than ele at jobs[pos][1]
-        auto nextItr = lower_bound(startTime.begin(), startTime.end(), jobs[pos][1]) - startTime.begin();
+        // Binary search: return iterator to first ele (startTime) which is >= than ele at jobs[pos][1] (job end time)
+        auto nextValidItr = lower_bound(startTime.begin(), startTime.end(), jobs[pos][1]) - startTime.begin();
         
         // Take it or leave it
-        int maxProfit = max(findMaxProfit(jobs, startTime, n, pos+1), jobs[pos][2] + findMaxProfit(jobs, startTime, n, nextItr));
+        // If you take current job, pass in next valid job idx which is done via binary search
+        int maxProfit = max(findMaxProfit(jobs, startTime, n, pos+1), jobs[pos][2] + findMaxProfit(jobs, startTime, n, nextValidItr));
 
         memo[pos] = maxProfit;
         return maxProfit;
