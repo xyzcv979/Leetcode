56643970ce0001058366c1ffb493043357ddf184
Alex <avon1462@gmail.com>
2022-11-26 16:11:14 -0500

    Time: 578 ms (9.53%), Space: 84 MB (6.97%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    map<int, int> memo;
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>> jobs;
+        vector<int> newStartTime(n);
+        vector<int> dp(n,0);
+        
+        for(int i = 0; i < n; i++) {
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        
+        sort(jobs.begin(), jobs.end());
+        
+        // Set sorted start times for binary search
+        for(int i = 0; i < n; i++) {
+            newStartTime[i] = jobs[i][0];
+        }
+        
+        return findMaxProfit(jobs, newStartTime, n, 0);
+    }
+    
+    int findMaxProfit(vector<vector<int>>& jobs, vector<int>& startTime, int n, int pos) {
+        if(pos == n)
+            return 0;
+        
+        if(memo.find(pos) != memo.end())
+            return memo[pos];
+        
+        // Binary search: return iterator to first ele which is >= than ele at jobs[pos][1]
+        auto nextItr = lower_bound(startTime.begin(), startTime.end(), jobs[pos][1]) - startTime.begin();
+        
+        // Take it or leave it
+        int maxProfit = max(findMaxProfit(jobs, startTime, n, pos+1), jobs[pos][2] + findMaxProfit(jobs, startTime, n, nextItr));
+
+        memo[pos] = maxProfit;
+        return maxProfit;
+    }
+};
+
+/*
+sort by start times first
+dp: take it or leave it
+binary search to optimize for searching for jobs that don't conflict with the job you picked
+linear search would take too long
+*/
