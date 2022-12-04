595dbfcb44f23e13087df4100362d0d17b0b5d45
Alex <avon1462@gmail.com>
2022-12-04 13:31:36 -0500

    Time: 257 ms (61.91%), Space: 78.4 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
         int n = nums.size();
-        vector<long long> prefix(n,0), suffix(n,0);
         int minDiff = INT_MAX;
         int minDiffIdx = 0;
+        long long totalSum = 0;
+        long long prefixSum = 0;
         
-        prefix[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i]; 
-        }
-        
-        suffix[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            suffix[i] = suffix[i+1] + nums[i];
+        for(int i : nums) {
+            totalSum += i;
         }
         
         for(int i = 0; i < n; i++) {
             int firstHalfCount = i + 1;
             int secHalfCount = n - i - 1;
-            long long firstHalfSum = prefix[i];
             long long secHalfSum;
+            
+            prefixSum += nums[i];
             
             // Last element
             if(i == n -1) {
                 secHalfSum = 0;
                 secHalfCount = 1;
             } else {
-                secHalfSum = suffix[i+1];
+                secHalfSum = totalSum - prefixSum;
             }
             
-            int diff = abs((firstHalfSum/firstHalfCount) - (secHalfSum/secHalfCount));
+            int diff = abs((prefixSum/firstHalfCount) - (secHalfSum/secHalfCount));
             if(diff < minDiff) {
                 minDiff = diff;
                 minDiffIdx = i;
